46d14c510d495efcfc5a87ece75299f11a872444
bonildav <david.angel.bonilla@gmail.com>
2021-06-01 23:53:39 +0000

    terraform: main.tf and provider tf
    First repo commit, adding a sample read me and some terraform code.
    Including first `main.tf` file and `provider.tf`

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "google" {
+  project = "learning-project-288414"
+  version = "3.70.0"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+  #credentials = file("/downloads/instance.json")
+}
